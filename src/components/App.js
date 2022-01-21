3d970d7ee58b3b60c22241e7d944b7003288b78a
Jennifer Tchai <jentchai@gmail.com>
2022-01-20 19:36:23 -0500

    commit tree 021e4f0dda1ae52482ae6dd9c36ea6789aae4b09

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [cate
+
+  function handleDeleteTask(deletedTaskText) {
+    setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+  }
+
+  function selectedCategory() {
+
+  }
+
+  function onSelectedCaterog
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} />
       <NewTaskForm />
-      <TaskList />
+      <TaskList handleDeleteTask={handleDeleteTask} tasks={TASKS} />
     </div>
   );
 }
